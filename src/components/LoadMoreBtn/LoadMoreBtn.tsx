1caaa2a327a2c022c5fab27d608693809ab2f90a
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-21 14:16:46 +0100

    as

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -4,4 +4,4 @@ const LoadMoreBtn: React.FC<LoadMoreType> = ({ onClick }: LoadMoreType) => {
   return <button onClick={onClick}>Load more</button>;
 };
 
-export default LoadMoreBtn;
+export default;
