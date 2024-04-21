f5200cc55821e485b5dd3ce7092799c81cf0fd27
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-21 17:08:42 +0100

    building

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,11 +4,8 @@ import css from "./Loader.module.css";
 const Loader: React.FC<_Props3> = () => {
   const defaultProps: _Props3 = {
     width: "96",
-    color: "grey",
     strokeWidth: "5",
     animationDuration: "0.75",
-    wrapperStyle: {},
-    wrapperClass: "",
     ariaLabel: "rotating-lines-loading",
   };
   return (
