70d4ad033155493d6c35197046eb2a52b3e5022f
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-21 16:52:16 +0100

    adds

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,10 +4,10 @@ import css from "./Loader.module.css";
 const Loader: React.FC<_Props3> = () => {
   const defaultProps: _Props3 = {
     height: 96,
-    width: 96,
+    width: "96",
     color: "grey",
-    strokeWidth: 5,
-    animationDuration: 0.75,
+    strokeWidth: "5",
+    animationDuration: "0.75",
     wrapperStyle: {},
     wrapperClass: "",
     ariaLabel: "rotating-lines-loading",
